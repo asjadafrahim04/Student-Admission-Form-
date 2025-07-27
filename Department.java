b8ea88121d2093b1eec3cf9c717adac5abd3063a
Asjad Afrahim <asjadafrahim.04@gmail.com>
2025-07-27 15:06:21 +0600

    Enum department

---
+++ Department.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author HP
+ */
+enum Department {
+    SWE, CSE, EEE, BBA, LLB, ECONOMICS, ENGLISH
+}
+
