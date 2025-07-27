bd837856998f7f0e1db5cd2cdb100a0a7a24f348
Asjad Afrahim <asjadafrahim.04@gmail.com>
2025-07-27 14:45:28 +0600

    Enum waiver

---
+++ Waiver.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author HP
+ */
+    enum Waiver {
+    NONE, FIVE_PERCENT, TEN_PERCENT, FIFTEEN_PERCENT, TWENTY_PERCENT, FIFTY_PERCENT, FULL
+}
+
+
+    
+
